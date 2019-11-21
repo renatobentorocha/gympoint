6c6cb684745fdd6da8cad10657721c0395882e45
Renato Rocha <renatob.rocha@gmail.com>
2019-11-21 14:07:27 -0300

    Prop types Indicator component

--- mobile/src/components/LoadIndicator/index.js
+++ mobile/src/components/LoadIndicator/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { ActivityIndicator } from 'react-native';
 
 import { Container } from './styles';
 
-export default function LoadIndicator({ size = 'large', color = '#ee4e62' }) {
+export default function LoadIndicator({ size, color }) {
   return (
     <Container>
       <ActivityIndicator size={size} color={color} />
     </Container>
   );
 }
+
+LoadIndicator.propTypes = {
+  size: PropTypes.string,
+  color: PropTypes.string,
+};
+
+LoadIndicator.defaultProps = {
+  size: 'large',
+  color: '#ee4e62',
+};
