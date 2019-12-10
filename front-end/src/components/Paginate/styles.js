d365f7a0b011418fc61b3fd8c151fcce6036412d
Renato Rocha <renatob.rocha@gmail.com>
2019-12-10 14:56:49 -0300

    Add margin top

--- front-end/src/components/Paginate/styles.js
+++ front-end/src/components/Paginate/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { darken } from 'polished';
 
 export const Container = styled.section`
+  margin-top: 2em;
   display: flex;
   align-items: center;
 
