9160610c922295057ed1d322ee497927f691c368
Renato Rocha <renatob.rocha@gmail.com>
2019-12-11 12:21:51 -0300

    Sentry configuration

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -3,6 +3,10 @@ import './bootstrap';
 import path from 'path';
 import express from 'express';
 import cors from 'cors';
+
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
+
 import routes from './routes';
 
 import './database';
@@ -11,11 +15,14 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(cors());
     this.server.use(
